7a343d76d29bd20e6f3636f8d535bdde4650f513
mosithean <mosithean@gmail.com>
2022-04-25 14:07:18 +0200

    Dockerize the sample application with GH-Actions

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+const express = require('express');
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const app = express();
+app.get('/', (req, res) => {
+    res.send(`Hello!<br><br>I see you’re from: ${req.ip}`);
+    console.log(`Request incoming from: ${req.ip}`);
+});
+app.listen(PORT, HOST);
+console.log(`Server running on http://${HOST}:${PORT}`);
